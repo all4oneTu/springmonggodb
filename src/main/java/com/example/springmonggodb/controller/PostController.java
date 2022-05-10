04897b43b7530502731a9680aeedcb9d91eaf56b
all4oneTu <81431586+all4oneTu@users.noreply.github.com>
2022-05-11 04:24:43 +0700

    tesst

--- src/main/java/com/example/springmonggodb/controller/PostController.java
+++ src/main/java/com/example/springmonggodb/controller/PostController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
+@CrossOrigin(origins="*", allowedHeaders="*")
 @RestController
 @RequestMapping("post")
 public class PostController {
